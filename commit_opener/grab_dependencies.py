5e484f9d5dc9b535a0ae80dad18ed6aff0bbc505
Craig MacLachlan <cs.maclachlan@gmail.com>
2016-03-23 14:39:40 +0000

    Removed start of line symbol from regex.

--- commit_opener/grab_dependencies.py
+++ commit_opener/grab_dependencies.py
@@ -72,9 +72,9 @@ def search_files_for_imports(repo_instance):
     
 def find_imports(text):
     """Apply regular expression searching to a file"""
-    # list of regexes
-    reexps = [re.compile(r'^\s+import\s+(\w+)', re.MULTILINE),
-              re.compile(r'^\s+from\s+(\w+)', re.MULTILINE)
+    # list of regexes. Strat of line "^" doesn't work for some reason.
+    reexps = [re.compile(r'\s+import\s+(\w+)', re.MULTILINE),
+              re.compile(r'\s+from\s+(\w+)', re.MULTILINE)
               ]
     import_list = []          
     for myregex in reexps:
