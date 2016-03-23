bf43684c8a030421cf338919c07bf5bc23649965
Billingham <laurence@bgs.ac.uk>
2016-03-23 14:10:02 +0000

    moving constants

--- commit_opener/commit_opener.py
+++ commit_opener/commit_opener.py
@@ -6,14 +6,8 @@ from shutil import rmtree
 
 from . tree_scrape import author_minded
 
-FAKE_CONTRIB_DATA = pd.Series(
-    index=['J. Doe'],
-    data={'1st':pd.datetime(2000, 1, 1),
-          'last':pd.datetime.now(),
-          'fraction':0.98}
-)
-
-OUT_FOLDER = 'contrib_data'
+OUT_SUBFOLDER = 'contrib_data'
+AUTHOR_DATA = 'author_data.json'
 
 def verify_local_repo_location(repo):
     if not os.path.isdir(repo):
@@ -22,7 +16,7 @@ def verify_local_repo_location(repo):
 def build_out_path(repo_name, parent_path=None):
     if parent_path is None:
         parent_path = os.path.abspath(os.curdir)
-    out_path = os.path.join(parent_path, repo_name, OUT_FOLDER)
+    out_path = os.path.join(parent_path, repo_name, OUT_SUBFOLDER)
     return out_path
 
 def make_output_folder(path_, overwrite):
@@ -32,14 +26,13 @@ def make_output_folder(path_, overwrite):
         rmtree(path_)
         os.mkdir(path_)
 
-
 @click.command()
-@click.option('--repo_dir', prompt='git repository location', help='path to folder containing  .git repository')
+@click.option('--repo_dir', prompt='git repository location',
+    help='path to folder containing  .git repository')
 @click.option('--out_dir', default=None,
     help='parent dir for output data, default same as .git folder scraped')
 @click.option('--clobber_output', default=True,
         help='should we overwrite existing data?, default True')
-
 def main(repo_dir, out_dir, clobber_output):
     """  """
     verify_local_repo_location(repo_dir)
@@ -47,7 +40,9 @@ def main(repo_dir, out_dir, clobber_output):
     outpath = build_out_path(repo_dir, out_dir)
     make_output_folder(outpath, overwrite=clobber_output)
     contributor_data = author_minded(repo_dir)
-    contributor_data.to_json(os.path.join(outpath,'contributor_data.json'))
+    contributor_data.to_json(os.path.join(outpath, AUTHOR_DATA))
+    print('wrote contribution data to {}'.format(outpath))
+    return
 
 
 if __name__ == '__main__':
