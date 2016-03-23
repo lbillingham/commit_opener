9f7d7500cdad5f13476acea2cba33798cd2b2e3c
Steve Harris (docsteveharris) <git@steveharris.me>
2016-03-23 14:08:35 +0000

    interim

--- query_pmc.py
+++ query_pmc.py
@@ -1,37 +1,76 @@
 # Query PMC
+# - [ ] TODO(2016-03-23): returns 1st 1000 only
+import pandas as pd
+import json
+import requests
+import collections
 
-def pmc_query(_query, _type="profile", _format="json"):
+def pmc_query(_query, _type="profile", _format="json", _page_size=10):
     # References
     # API https://europepmc.org/RestfulWebService#search
     # Search tags https://europepmc.org/Help#directsearch
-    import requests
     # Construct query string
     _europmc = "http://www.ebi.ac.uk/europepmc/webservices/rest"
     _search = "/search?query="
     _profile = "/profile?query="
     _format = "&format=json"
-
     if _type == "profile":
         _url = _europmc + _profile + _query + _format
     elif _type == "search":
-        _url = _europmc + _search + _query + _format
+        _url = _europmc + _search + _query + _format + "&pageSize=" + str(_page_size)
     else:
         print("_type not one of 'search' or 'profile'")
         return
-
     resp = requests.get(_url)
-    return resp
-
-# full text search (AND combination)
-# resp = pmc_query("ghorashian harris", _type="search")
-
-# example of tagged search
-# resp = pmc_query("auth:ghorashian")
-
-# example of AND search with tags
-# resp = pmc_query("auth:ghorashian pub_year:2010")
-
-# resp = pmc_query("ghorashian harris", _type="profile")
-# print resp.text.encode('ascii', 'ignore')
+    return resp.json()
 
 
+def pmc_field_filter(_pmc_response, _fields=['id', 'pubYear', 'citedByCount']):
+    # Extract the results from the search (needs to be passed a search not a profile object)
+    _pmc_dict = _pmc_response["resultList"]["result"]
+    def _filter(_dict, _fields=_fields):
+        _filtered = dict(((k, v) for k, v in _dict.items() if k in _fields))
+        return _filtered
+    _pmc_filtered  = [_filter(i) for i in _pmc_dict]
+    return _pmc_filtered
+
+def dict_to_df(_dict):
+    # Convert list of dictionaries to dict of lists
+    result = collections.defaultdict(list)
+    for d in _dict:
+        for k, v in d.items():
+            result[k].append(v)
+    return result
+
+def pmc_data(_search_string):
+    resp = pmc_query(_search_string, _type="profile")
+    pmc_hits = 
+    resp = pmc_query(_search_string, _type="search")
+    pmc_citations = pmc_field_filter(resp)
+print pmc_citations
+
+dict_to_df(pmc_citations)
+
+resp = pmc_query("spss", _type="profile")
+print json.dumps(resp, ensure_ascii=True, indent=4)
+
+    
+# pd.DataFrame(result["citedByCount"], index=result["id"])
+df = pd.DataFrame(result)
+
+# Check number from profile
+
+# Run a search
+resp = pmc_query("spss", _type="search")
+print json.dumps(resp, ensure_ascii=True, indent=4)
+
+# print pmc_field_filter(resp)
+# print len(pmc_field_filter(resp))
+pmc_citations = pmc_field_filter(resp)
+print pmc_citations
+
+dict_to_df(pmc_citations)
+
+return df
+
+if __name__ == "__main__":
